16c0b50450ab72f4796f04c6812ca9aa9a1a05ba
DanVev <vasily.danilin@yandex.ru>
2017-09-10 14:53:16 +0300

    move test image to test_images folder

--- visual/faceDetection.py
+++ visual/faceDetection.py
@@ -5,7 +5,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(r'.\cascades\haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(r'.\cascades\haarcascade_eye.xml')
 
-img = cv2.imread('test_photo.jpg')
+img = cv2.imread(r'.\test_images\test_photo.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
 for (x, y, w, h) in faces:
